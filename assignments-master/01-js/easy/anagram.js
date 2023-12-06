9808fe36375d83fd45add814985619f5a350493a
monkCommits <speak2monk@gmailcom>
2023-12-06 12:07:49 +0530

    easy-01Solved

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,30 @@
 */
 
 function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+  if(str1.length!==str2.length){
+    return false
+  }
+  else{
+    for(let i=0; i<str1.length;i++){
+      let j=0
+      while(j<str1.length){
+        
+        if(str1[i]===str2[j]){
 
+          str2[j]='0'
+          break
+        }
+        j++
+      }
+      if(j===str1.length){
+
+        return false
+      }
+    }
+  }
+  return true
 }
-
+console.log(isAnagram('name','mane'));
 module.exports = isAnagram;
